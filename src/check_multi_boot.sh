48194ab511289e27f3914483af4e1e15cfa3129c
Tomas <tozaicevas@gmail.com>
2020-02-27 15:57:18 +0200

    minor changes

--- src/check_multi_boot.sh
+++ src/check_multi_boot.sh
@@ -1,5 +1,5 @@
 if grub-file --is-x86-multiboot2 ../bin/kernel.elf; then
   echo multiboot2 confirmed
 else
-  echo kernel.bin is not multiboot2 compliant
+  echo kernel.elf is not multiboot2 compliant
 fi
