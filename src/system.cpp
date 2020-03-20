0160074f9ddf9ea97f7b8b5dbc85d23fb87a904f
Tomas <tozaicevas@gmail.com>
2020-03-20 18:53:06 +0200

    outb, inb bug fixed

--- src/system.cpp
+++ src/system.cpp
@@ -34,12 +34,12 @@ void *memsetw(uint16_t *dest, uint16_t val, uint64_t n) {
 
 uint16_t inb(uint16_t port) {
 	uint16_t val;
-	asm volatile ("in %1, %0" : : "Nd"(port), "a"(val) );
+	asm volatile ("inb %0, %1" : : "a"(val), "Nd"(port));
 	return val;
 }
 
 void outb(uint16_t port, uint8_t val) {
-	asm volatile ("out %1, %0" : : "a"(val), "Nd"(port) );
+	asm volatile ("outb %0, %1" : : "Nd"(port), "a"(val));
 }
 
 void halt() {
