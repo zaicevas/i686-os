bde87fa1a226af079099efb777b67eebefb7a595
Tomas <tozaicevas@gmail.com>
2020-06-07 20:36:56 +0300

    simplest paging setup

--- src/paging.cpp
+++ src/paging.cpp
@@ -3,6 +3,8 @@
 #include <system.h>
 #include <memory.h>
 #include <pic.h>
+#include <string.h>
+#include <debug.h>
 
 #define INDEX_FROM_BIT(a) (a/(8*4))
 #define OFFSET_FROM_BIT(a) (a%(8*4))
@@ -13,6 +15,10 @@ using namespace memory;
 typedef unsigned int uword_t __attribute__ ((mode (__word__)));
 
 namespace paging {
+
+    uint32_t page_directory[1024] __attribute__((aligned(4096)));
+    uint32_t first_page_table[1024] __attribute__((aligned(4096)));
+
     uint64_t placement_address = 0;
 
     // The kernel's page directory
@@ -78,6 +84,9 @@ namespace paging {
                 PANIC("No free frames!");
             }
             set_frame(idx*0x1000);
+            // qemu_printf("setting frame at: ");
+            // qemu_printf(itoa(idx * 0x1000));
+            // qemu_printf("\n");
             page->present = 1;
             page->rw = (is_writeable)?1:0;
             page->user = (is_kernel)?0:1;
@@ -99,6 +108,22 @@ namespace paging {
 
     void init() {
         paging::placement_address = (uint32_t) &kernel_ends_at;
+
+        unsigned int i;
+
+        memset((uint8_t*)page_directory, 0, 4 * 1024);
+        memset((uint8_t*)first_page_table, 0, 4 * 1024);
+        
+        //we will fill all 1024 entries in the table, mapping 4 megabytes
+        for (i = 0; i < 1024; i++) {
+            // As the address is page aligned, it will always leave 12 bits zeroed.
+            // Those bits are used by the attributes ;)
+            first_page_table[i] = (i * 0x1000) | 3; // attributes: supervisor level, read/write, present.
+        }
+
+        page_directory[0] = ((unsigned int)first_page_table) | 3;
+
+/*
         // The size of physical memory. For the moment we 
         // assume it is 16MB big.
         // TODO: find the end of RAM
@@ -119,14 +144,19 @@ namespace paging {
         // inside the loop body we actually change placement_address
         // by calling kmalloc(). A while loop causes this to be
         // computed on-the-fly rather than once at the start.
-        int i = 0;
-        while (i < placement_address) {
-            // Kernel code is readable but not writeable from userspace.
-            alloc_frame( get_page(i, 1, kernel_directory), 0, 0);
-            i += 0x1000;
-        }
+        // uint32_t i = 0;
+        // while (i < placement_address * 2) {
+        //     // Kernel code is readable but not writeable from userspace.
+        //     alloc_frame( get_page(i, 1, kernel_directory), 0, 0);
+        //     i += 0x1000;
+        //     qemu_printf("i: ");
+        //     qemu_printf(itoa(i));
+        //     qemu_printf("\n");
+        // }
         // Before we enable paging, we must register our page fault handler.
         // pic::set_gate(14, (uint32_t) &page_fault);
+
+        */
         pic::register_interrupt_handler(14, (uint32_t) page_fault);
 
         // Now, enable paging!
@@ -134,8 +164,9 @@ namespace paging {
     }
 
     void switch_page_directory(page_directory_t *dir) {
-        current_directory = dir;
-        asm volatile("mov %%cr3, %0":: "r"(&dir->tablesPhysical)); // write to cr3
+        // current_directory = dir;
+        asm volatile("mov %%cr3, %0":: "r"(page_directory)); // write to cr3
+        // while(true) {}
         uint32_t cr0 = read_cr0();
         cr0 |= 0x80000000; // Enable paging!
         write_cr0(cr0);
@@ -146,12 +177,19 @@ namespace paging {
         address /= 0x1000;
         // Find the page table containing this address.
         uint32_t table_idx = address / 1024;
-        if (dir->tables[table_idx]) { // If this table is already assigned
-            return &dir->tables[table_idx]->pages[address%1024];
-        }
-        else if(make) {
+        // if (dir->tables[table_idx]) { // If this table is already assigned
+        // qemu_printf("address: ");
+        // qemu_printf(itoa(address));
+        // qemu_printf("\n");
+        //     // qemu_printf("already assigned");
+        //     // qemu_printf(itoa((uint32_t) &dir->tables[table_idx]->pages[address%1024]));
+        //     return &dir->tables[table_idx]->pages[address%1024];
+        // }
+        if (make) {
+            qemu_printf("what");
             uint32_t tmp;
             dir->tables[table_idx] = (page_table_t*)kmalloc_ap(sizeof(page_table_t), &tmp);
+            qemu_printf("yes im there");
             dir->tablesPhysical[table_idx] = tmp | 0x7; // PRESENT, RW, US.
             return &dir->tables[table_idx]->pages[address%1024];
         }
