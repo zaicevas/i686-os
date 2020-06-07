84ea5189afa9250af66c2b62c8ea342f36cfe555
Tomas <tozaicevas@gmail.com>
2020-06-07 15:38:40 +0300

    paging 2

--- src/include/pic.h
+++ src/include/pic.h
@@ -10,6 +10,9 @@ namespace pic {
     void set_gate(uint8_t index, uint32_t address); 
     void unmask(uint8_t irq); // irq = maskable hardware interrupt [0, 15]
     void mask(uint8_t irq); 
+    
+    // Add additional handler that will be called when interrupt happens
+    void register_interrupt_handler(uint8_t index, uint32_t handler_address);
 }
 
 #endif
