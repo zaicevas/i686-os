012554e7fb349663a5d11b62e7ed247c15be9da6
Tomas <tozaicevas@gmail.com>
2020-06-10 13:06:33 +0300

    some helper string.h functions implemented

--- src/include/string.h
+++ src/include/string.h
@@ -7,4 +7,8 @@ uint32_t strlen(const char *s);
 
 char *itoa(uint64_t val);
 
+bool are_strings_equal(const char *lhs, const char *rhs);
+
+char *trim(const char *s);
+
 #endif
--- src/string.cpp
+++ src/string.cpp
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <memory.h>
 
 uint32_t strlen(const char *s) {
     uint32_t len = 0;
@@ -20,4 +21,32 @@ char *itoa(uint64_t val) {
 		buf[i] = "0123456789abcdef"[val % BASE];
 	
 	return &buf[i+1];
+}
+
+bool are_strings_equal(const char *lhs, const char *rhs) {
+	uint32_t strlen_lhs = strlen(lhs);
+	uint32_t strlen_rhs = strlen(rhs);
+	if (strlen_lhs != strlen_rhs)
+		return false;
+	for (uint32_t i=0; i<strlen_lhs; i++) {
+		if (lhs[i] != rhs[i])
+			return false;
+	}
+	return true;
+}
+
+static bool is_white_space(char c) {
+	return c <= ' ' || c >= 0x7f;
+}
+
+char *trim(const char *s) {
+	uint32_t length = strlen(s);
+	char *trimmed = (char *) memory::kmalloc(length);
+	uint32_t j = 0;
+	for (uint32_t i=0; i<strlen(s); i++) {
+		if (!is_white_space(s[i]))
+			trimmed[j++] = s[i];
+	}
+	trimmed[j] = 0;
+	return trimmed;
 }
